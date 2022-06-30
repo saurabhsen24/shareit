88728dead5338502610a51382267f4ffdb42bd02
Saurabh Sen <saurabhsensaurabh@gmail.com>
2022-06-30 18:10:40 +0530

    Adds Authorization check on user feature

--- backend/src/main/java/com/shareit/controller/UserController.java
+++ backend/src/main/java/com/shareit/controller/UserController.java
@@ -7,6 +7,7 @@ import com.shareit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,12 +18,14 @@ public class UserController {
     private UserService userService;
 
     @DeleteMapping("/removeUser/{userId}")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
     public ResponseEntity<GenericResponse> removeUser(@PathVariable("userId") Long userId){
         userService.deleteUser(userId);
         return new ResponseEntity<>(new GenericResponse("User Deleted Successfully"), HttpStatus.OK);
     }
 
     @PatchMapping("/update")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.updateUser(userDto), HttpStatus.OK);
     }
